82bfe0acca6d9b47e1a2e7b742606059171cf939
alexlashford <alexl@cakesolutions.net>
2017-01-22 00:28:05 +0000

    first cut with working countdown

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -6,7 +6,39 @@
     .controller('HomeController', HomeController);
 
   /** @ngInject */
-  function HomeController() {
-    // var vm = this;
+  function HomeController($interval) {
+
+    var vm = this;
+
+    function initializeClock(endtime) {
+
+      function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        var days = Math.floor(t / (1000 * 60 * 60 * 24));
+        return {
+          'total': t,
+          'days': days,
+          'hours': hours,
+          'minutes': minutes,
+          'seconds': seconds
+        };
+      }
+
+      function updateClock() {
+        vm.countdown = getTimeRemaining(endtime);
+        if (vm.countdown <= 0) {
+          vm.timer.stop();
+        }
+      }
+
+      updateClock(); // do the first call
+      vm.timer = $interval(updateClock, 1000); // call function every second
+    }
+
+    initializeClock(new Date('February 24 2018 12:00:00'));
+
   }
 })();
